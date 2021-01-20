b5a4228e2ceaf783d552ccbfd25e150b65217669
Gyebro <gyebroszkigergely@gmail.com>
2021-01-20 13:45:57 +0100

    Using 3D transform between origin and robot

--- fiducial_tracker/src/fiducial_tracker.cpp
+++ fiducial_tracker/src/fiducial_tracker.cpp
@@ -15,6 +15,8 @@ ros::Publisher marker_pub;
 ros::Publisher odom_pub;
 
 int robot_id  = 0;
+int ground_id = 1;
+bool use_ground_marker = false;
 std::string odom_frame_id;
 
 geometry_msgs::Transform roboTransform;
@@ -26,7 +28,7 @@ tf::Transform robot_TF_origo;
 
 bool originHasBeenSet = false;
 bool resetRobotOrigin(ros_fiducial_tracker::resetOrigin::Request  &req,
-                      ros_fiducial_tracker::resetOrigin::Response &res){
+                      ros_fiducial_tracker::resetOrigin::Response &res) {
   roboTransformOrigo = roboTransform;
   res.result = originHasBeenSet = req.resetOrigin;
   if (originHasBeenSet) ROS_INFO("Origin set.");
@@ -34,61 +36,78 @@ bool resetRobotOrigin(ros_fiducial_tracker::resetOrigin::Request  &req,
   return true;
 }
 
-// TODO: marker TF
+void update_messages() {
+  if (originHasBeenSet) {
+    tf::transformMsgToTF(roboTransform, robot_TF);
+    tf::transformMsgToTF(roboTransformOrigo, robot_TF_origo);
+    tf::Transform dislocation = robot_TF_origo.inverseTimes(robot_TF);
+    tf::transformTFToMsg(dislocation, odomTransform);
+    
+    //double robotHeading = tf::getYaw(odomTransform.rotation);
+    //tf::quaternionTFToMsg(tf::createQuaternionFromYaw(robotHeading),odomTransform.rotation);
+
+    // Visualization marker
+    visualization_msgs::Marker points;
+    points.header.frame_id = "odom";
+    points.header.stamp    = ros::Time::now();
+    points.ns = "fiducial_tracker";
+    points.action = visualization_msgs::Marker::ADD;
+    points.id = robot_id;
+    points.pose.orientation = odomTransform.rotation;
+    points.type = visualization_msgs::Marker::POINTS;
+    points.scale.x = 0.1;
+    points.scale.y = 0.1;
+    points.color.g = 1.0f;
+    points.color.a = 1.0f;
+    geometry_msgs::Point p;
+    p.x = odomTransform.translation.x;
+    p.y = odomTransform.translation.y;
+    p.z = odomTransform.translation.z;
+    points.points.push_back(p);
+    
+    // Odometry
+    nav_msgs::Odometry odom;
+    odom.child_frame_id = odom_frame_id;
+    odom.header.stamp = ros::Time::now();
+    odom.header.frame_id = "odom";
+    odom.pose.pose.position.x  = odomTransform.translation.x;
+    odom.pose.pose.position.y  = odomTransform.translation.y;
+    odom.pose.pose.position.z  = odomTransform.translation.z;
+    odom.pose.pose.orientation = odomTransform.rotation;
+    // TODO: calculate velocities
+
+    // Publish messages
+    marker_pub.publish(points);
+    odom_pub.publish(odom);
+  }
+}
+
 void fiducialTransFormArrayUpdate(const fiducial_msgs::FiducialTransformArray &ftf_array){
   auto array_length = std::size(ftf_array.transforms);
-  if (array_length > 0)
-  {
-    for (uint8_t i = 0; i < array_length; i++)
-    {
-      if (ftf_array.transforms[i].fiducial_id == robot_id)
-      {
-        roboTransform = ftf_array.transforms[i].transform;
-
-        if (originHasBeenSet)
-        {
-          tf::transformMsgToTF(roboTransform,robot_TF);
-          tf::transformMsgToTF(roboTransformOrigo,robot_TF_origo);
-          tf::Transform dislocation = robot_TF_origo.inverseTimes(robot_TF);
-          tf::transformTFToMsg(dislocation, odomTransform);
-          double robotHeading = tf::getYaw(odomTransform.rotation);
-          tf::quaternionTFToMsg(tf::createQuaternionFromYaw(robotHeading),odomTransform.rotation);
-
-          // VISUALIZATION MARKER
-          visualization_msgs::Marker points;
-          points.header.frame_id = "odom";
-          points.header.stamp    = ros::Time::now();
-          points.ns = "fiducial_tracker";
-          points.action = visualization_msgs::Marker::ADD;
-          points.id = robot_id;
-          points.pose.orientation.w = 1;
-          points.type = visualization_msgs::Marker::POINTS;
-          points.scale.x = 0.1;
-          points.scale.y = 0.1;
-          points.color.g = 1.0f;
-          points.color.a = 1.0;
-          geometry_msgs::Point p;
-          p.x = odomTransform.translation.x;
-          p.y = odomTransform.translation.y;
-          p.z = 0;
-          points.points.push_back(p);
-          marker_pub.publish(points);
-
-          // ODOMETRY
-          nav_msgs::Odometry odom;
-          odom.child_frame_id = odom_frame_id;
-          odom.header.stamp = ros::Time::now();
-          odom.header.frame_id = "odom";
-          odom.pose.pose.position.x  = odomTransform.translation.x;
-          odom.pose.pose.position.y  = odomTransform.translation.y;
-          odom.pose.pose.position.z  = 0;
-          odom.pose.pose.orientation = odomTransform.rotation;
-          // TODO: calculate velocities
-
-          // publish the message
-          odom_pub.publish(odom);
-        }
+  int ground_idx = -1;
+  int robot_idx = -1;
+  if (array_length > 0) {
+    // Check for ground and robot markers
+    for (uint8_t i = 0; i < array_length; i++) {  
+      if (ftf_array.transforms[i].fiducial_id == ground_id) {
+        ground_idx = i;
+      } else if (ftf_array.transforms[i].fiducial_id == robot_id) {
+        robot_idx = i;
       }
+    }
+    // If ground marker is present, update its transform
+    if (ground_idx >= 0) {
+      roboTransformOrigo = ftf_array.transforms[ground_idx].transform;
+      if (!originHasBeenSet) {
+        originHasBeenSet = true;
+        ROS_INFO_STREAM("Origin set using ground marker (ID = " << ground_id << ")");
+      }
+    }
+    // If robot marker is present, update its transform and publish updated messages
+    if (robot_idx >= 0) {
+      ROS_INFO("Robot marker is present");
+      roboTransform = ftf_array.transforms[robot_idx].transform;
+      update_messages();
     }
   }
 }
@@ -106,6 +125,12 @@ int main(int argc, char ** argv) {
       ROS_WARN("Failed to get param 'robot_id' - default: 0");
       robot_id = 0;
     }
+    
+    if (ros::param::get("~ground_id", ground_id)) {}
+    else {
+      ROS_WARN("Failed to get param 'ground_id' - default: 1");
+      ground_id = 1;
+    }
 
     if (ros::param::get("~odom_frame_id", odom_frame_id)) {}
     else {
@@ -113,7 +138,7 @@ int main(int argc, char ** argv) {
       odom_frame_id = "base_link";
     }
 
-    ROS_INFO("Waiting for robot origin...");
+    ROS_INFO("Waiting for robot origin or ground marker to appear...");
     ros::spin();
     return 0;
 }
