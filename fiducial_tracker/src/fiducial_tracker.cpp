8cfd8770c08677fad6a24a3f3a381ac6ab03d81f
Marton Juhasz <marton@nowtech.hu>
2021-01-21 13:49:41 +0100

    tf broadcaster

--- fiducial_tracker/src/fiducial_tracker.cpp
+++ fiducial_tracker/src/fiducial_tracker.cpp
@@ -17,13 +17,14 @@
 ros::Publisher marker_pub;
 ros::Publisher odom_pub;
 
-std::string odom_frame_id;
+std::string origin_frame_id, camera_frame_id, robot_frame_id;
 std::string node_ns;
 
-int robot_id  = 0;
-int ground_id = 1;
+int origin_id = 0;
+int robot_id  = 1;
 bool use_ground_marker = false;
 bool publish_visualisation_markers = false;
+bool publish_fiducial_tf = true;
 
 geometry_msgs::Transform roboTransform;
 geometry_msgs::Transform roboTransformOrigo;
@@ -33,6 +34,8 @@ geometry_msgs::Transform robotDisplacementTransform;
 tf::Transform robot_TF;
 tf::Transform robot_TF_prev;
 tf::Transform robot_TF_origo;
+
+tf2_ros::TransformBroadcaster* broadcaster = NULL;
 
 ros::Time twist_timestamp;
 
@@ -46,7 +49,7 @@ bool resetRobotOrigin(ros_fiducial_tracker::resetOrigin::Request  &req,
   return true;
 }
 
-void update_messages()
+void update_messages(ros::Time message_time)
 {
 
   if (originHasBeenSet)
@@ -65,8 +68,8 @@ void update_messages()
 
     if (publish_visualisation_markers)
     {
-      points.header.frame_id = odom_frame_id;
-      points.header.stamp    = ros::Time::now();
+      points.header.frame_id = origin_frame_id;
+      points.header.stamp    = message_time;
       points.ns = node_ns;
       points.action = visualization_msgs::Marker::ADD;
       points.id = robot_id;
@@ -84,11 +87,12 @@ void update_messages()
       marker_pub.publish(points);
     }
 
-    double twist_dt_sec = (odom.header.stamp-twist_timestamp).toSec();
+    double twist_dt_sec = (message_time-twist_timestamp).toSec();
+    twist_timestamp = message_time;
 
-    odom.child_frame_id = odom_frame_id;
-    odom.header.stamp = ros::Time::now();
-    odom.header.frame_id = odom_frame_id;
+    odom.header.frame_id = origin_frame_id;
+    odom.header.stamp = message_time;
+    odom.child_frame_id = origin_frame_id; // !!!
     odom.pose.pose.position.x  = odomTransform.translation.x;
     odom.pose.pose.position.y  = odomTransform.translation.y;
     odom.pose.pose.position.z  = odomTransform.translation.z;
@@ -96,34 +100,52 @@ void update_messages()
     odom.twist.twist.linear.x  = robotDisplacementTransform.translation.x / twist_dt_sec;
     odom.twist.twist.linear.y  = robotDisplacementTransform.translation.y / twist_dt_sec;
     odom.twist.twist.linear.z  = robotDisplacementTransform.translation.z / twist_dt_sec;
-
-    twist_timestamp = odom.header.stamp;
     odom_pub.publish(odom);
+
+    if (publish_fiducial_tf)
+    {
+      geometry_msgs::TransformStamped transformStamped;
+    	transformStamped.header.stamp = message_time;
+    	transformStamped.header.frame_id = camera_frame_id;
+    	transformStamped.child_frame_id = robot_frame_id;
+    	transformStamped.transform.translation.x = roboTransform.translation.x;
+    	transformStamped.transform.translation.y = roboTransform.translation.y;
+    	transformStamped.transform.translation.z = roboTransform.translation.z;
+    	transformStamped.transform.rotation = roboTransform.rotation;
+    	broadcaster->sendTransform(transformStamped);
+    	transformStamped.child_frame_id = origin_frame_id;
+    	transformStamped.transform.translation.x = roboTransformOrigo.translation.x;
+    	transformStamped.transform.translation.y = roboTransformOrigo.translation.y;
+    	transformStamped.transform.translation.z = roboTransformOrigo.translation.z;
+    	transformStamped.transform.rotation = roboTransformOrigo.rotation;
+    	broadcaster->sendTransform(transformStamped);
+    }
+
   }
 }
 
 void fiducialTransFormArrayUpdate(const fiducial_msgs::FiducialTransformArray &ftf_array)
 {
   auto array_length = std::size(ftf_array.transforms);
-  int ground_idx = -1;
+  int origin_idx = -1;
   int robot_idx = -1;
   if (array_length > 0) {
     // Check for ground and robot markers
     for (uint8_t i = 0; i < array_length; i++) {
-      if (ftf_array.transforms[i].fiducial_id == ground_id) {
-        ground_idx = i;
+      if (ftf_array.transforms[i].fiducial_id == origin_id) {
+        origin_idx = i;
       } else if (ftf_array.transforms[i].fiducial_id == robot_id) {
         robot_idx = i;
       }
     }
     // If ground marker is present, update its transform
     bool update_required = false;
-    if (ground_idx >= 0) {
-      roboTransformOrigo = ftf_array.transforms[ground_idx].transform;
+    if (origin_idx >= 0) {
+      roboTransformOrigo = ftf_array.transforms[origin_idx].transform;
       update_required = true;
       if (!originHasBeenSet) {
         originHasBeenSet = true;
-        ROS_INFO_STREAM("Origin set using ground marker (ID = " << ground_id << ")");
+        ROS_INFO_STREAM("Origin set using ground marker (ID = " << origin_id << ")");
       }
     }
     // If robot marker is present, update its transform and publish updated messages
@@ -131,7 +153,7 @@ void fiducialTransFormArrayUpdate(const fiducial_msgs::FiducialTransformArray &f
       update_required = true;
       roboTransform = ftf_array.transforms[robot_idx].transform;
     }
-    if (update_required) update_messages();
+    if (update_required) update_messages(ftf_array.header.stamp);
   }
 }
 
@@ -139,13 +161,18 @@ int main(int argc, char ** argv)
 {
     ros::init(argc, argv, "fiducial_tracker");
     node_ns = ros::this_node::getName();
+
     ros::NodeHandle n;
     ros::NodeHandle nhLocal("~");
+    broadcaster = new(tf2_ros::TransformBroadcaster);
 
-    nhLocal.param("ground_id", ground_id, 0);
+    nhLocal.param("origin_id", origin_id, 0);
     nhLocal.param("robot_id", robot_id, 1);
-    nhLocal.param("odom_frame", odom_frame_id, std::string("fiducial_0"));
+    nhLocal.param("odom_frame", origin_frame_id, std::string("origin"));
+    nhLocal.param("camera_frame", camera_frame_id, std::string("camera"));
+    nhLocal.param("robot_frame", robot_frame_id, std::string("robot"));
     nhLocal.param("publish_markers", publish_visualisation_markers, false);
+    nhLocal.param("publish_fiducial_tf", publish_fiducial_tf, true);
 
     ros::Subscriber ftf_sub    = n.subscribe("/fiducial_transforms", 100, fiducialTransFormArrayUpdate);
     ros::ServiceServer service = n.advertiseService("/fiducial_tracker/reset_origin", resetRobotOrigin);
