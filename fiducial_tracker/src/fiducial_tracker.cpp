4484fed290dbc427cc78611a6edaa942b07c7fa5
Marton Juhasz <marton@nowtech.hu>
2021-01-23 12:13:22 +0100

    variable naming clarification

--- fiducial_tracker/src/fiducial_tracker.cpp
+++ fiducial_tracker/src/fiducial_tracker.cpp
@@ -26,41 +26,44 @@ int robot_id  = 1;
 
 bool publish_visualisation_markers = false;
 bool publish_fiducial_tf = true;
-bool wait_for_urdf = false;
+bool publish_odom_tf = false;
+bool publish_odometry = true;
+bool marker_to_footprint_from_urdf = false;
 
 geometry_msgs::Transform markerToFootprint;
-geometry_msgs::Transform roboTransform;
-geometry_msgs::Transform roboTransformPrev;
-geometry_msgs::Transform roboTransformOrigo;
+geometry_msgs::Transform markerToCamera;
+geometry_msgs::Transform markerToCameraPrev;
+geometry_msgs::Transform transformOrigin;
 geometry_msgs::Transform odomTransform;
 geometry_msgs::Transform robotDisplacementTransform;
 
-tf::Transform robot_TF;
-tf::Transform robot_TF_prev;
-tf::Transform robot_TF_origo;
+tf::Transform marker_to_cam;
+tf::Transform marker_to_cam_prev;
+tf::Transform transform_origin_tf;
 
 tf2_ros::TransformBroadcaster* tf_broadcaster = NULL;
 
 ros::Time twist_timestamp;
 
-bool originHasBeenSet  = false;
+bool trackingEnabled  = false;
 bool originNeedsUpdate = false;
 
 bool resetRobotOrigin(ros_fiducial_tracker::resetOrigin::Request  &req,
-                      ros_fiducial_tracker::resetOrigin::Response &res) 
+                      ros_fiducial_tracker::resetOrigin::Response &res)
 {
-  roboTransformOrigo = roboTransform;
-  res.result = originHasBeenSet = req.resetOrigin;
-  if (originHasBeenSet) 
+  if (req.resetOrigin)
   {
     originNeedsUpdate = true;
     ROS_INFO("Origin reset requested.");
   }
-  else ROS_INFO("Origin dropped. Tracking disabled.");
+  else {
+    ROS_INFO("Origin dropped. Tracking disabled.");
+    trackingEnabled = false;
+  }
   return true;
 }
 
-geometry_msgs::Vector3 update_angular_velocity(geometry_msgs::Quaternion q, geometry_msgs::Quaternion q0, double dt) 
+geometry_msgs::Vector3 update_angular_velocity(geometry_msgs::Quaternion q, geometry_msgs::Quaternion q0, double dt)
 {
   // Note: Maybe use tf::Quaternion and use built-in methods
   // Calculate q-dot (time derivative of quaternion)
@@ -74,7 +77,7 @@ geometry_msgs::Vector3 update_angular_velocity(geometry_msgs::Quaternion q, geom
   double dz = (z-q0.z)/dt;
   // Calculate omega (Qw) from q-dot and q
   double denom = ((dw*dw)*(w*w) + 2*dw*w*(dx*x + dy*y) + (dx*x + dy*y)*(dx*x + dy*y) + (dz*dz)*((w*w) + (x*x) + (y*y)));
-  double Qwx = (-2*dz*(dw*dy*w - dx*dz*w + dx*dy*x + dw*dz*x + ((dy*dy) + (dz*dz))*y))/denom;  
+  double Qwx = (-2*dz*(dw*dy*w - dx*dz*w + dx*dy*x + dw*dz*x + ((dy*dy) + (dz*dz))*y))/denom;
   double Qwy = (2*dz*(dw*dx*w + dy*dz*w + (dx*dx)*x + (dz*dz)*x + dx*dy*y - dw*dz*y))/denom;
   double Qwz = (2*dz*((dw*dw)*w + dz*(dz*w - dy*x + dx*y) + dw*(dx*x + dy*y)))/denom;
   // Rotate omega with q
@@ -91,19 +94,20 @@ geometry_msgs::Vector3 update_angular_velocity(geometry_msgs::Quaternion q, geom
 void update_messages(ros::Time message_time)
 {
 
-  if (originHasBeenSet)
+  if (trackingEnabled)
   {
 
     nav_msgs::Odometry odom;
     visualization_msgs::Marker points;
 
-    tf::transformMsgToTF(roboTransform, robot_TF);
-    tf::Transform robot_displacement = robot_TF.inverseTimes(robot_TF_prev);
-    
-    tf::transformMsgToTF(roboTransformOrigo, robot_TF_origo);
-    tf::Transform dislocation = robot_TF_origo.inverseTimes(robot_TF);
-    tf::transformTFToMsg(dislocation, odomTransform);
-    tf::transformTFToMsg(robot_displacement, robotDisplacementTransform);
+    tf::transformMsgToTF(markerToCamera, marker_to_cam);
+    tf::Transform marker_displacement_tf = marker_to_cam.inverseTimes(marker_to_cam_prev); // between two frames
+
+    tf::transformMsgToTF(transformOrigin, transform_origin_tf);
+    tf::Transform marker_to_origin_tf = transform_origin_tf.inverseTimes(marker_to_cam);
+
+    tf::transformTFToMsg(marker_to_origin_tf, odomTransform);
+    tf::transformTFToMsg(marker_displacement_tf, robotDisplacementTransform);
 
     if (publish_visualisation_markers)
     {
@@ -126,46 +130,66 @@ void update_messages(ros::Time message_time)
       marker_pub.publish(points);
     }
 
-    double twist_dt_sec = (message_time-twist_timestamp).toSec();
-    geometry_msgs::Vector3 omega = update_angular_velocity(roboTransform.rotation, roboTransformPrev.rotation, twist_dt_sec);
-    // Update "previous values" with current
-    twist_timestamp = message_time;
-    robot_TF_prev = robot_TF;
-    roboTransformPrev = roboTransform;
+    if (publish_odometry)
+    {
+      double twist_dt_sec = (message_time-twist_timestamp).toSec();
+      geometry_msgs::Vector3 omega = update_angular_velocity(markerToCamera.rotation, markerToCameraPrev.rotation, twist_dt_sec);
 
-    odom.header.frame_id = origin_frame_id;
-    odom.header.stamp = message_time;
-    odom.child_frame_id = marker_frame_id; // !!!
-    odom.pose.pose.position.x  = odomTransform.translation.x;
-    odom.pose.pose.position.y  = odomTransform.translation.y;
-    odom.pose.pose.position.z  = odomTransform.translation.z;
-    odom.pose.pose.orientation = odomTransform.rotation;
-    odom.twist.twist.linear.x  = robotDisplacementTransform.translation.x / twist_dt_sec;
-    odom.twist.twist.linear.y  = robotDisplacementTransform.translation.y / twist_dt_sec;
-    odom.twist.twist.linear.z  = robotDisplacementTransform.translation.z / twist_dt_sec;
-    odom.twist.twist.angular.x = omega.x;
-    odom.twist.twist.angular.y = omega.y;
-    odom.twist.twist.angular.z = omega.z;
-    odom_pub.publish(odom);
+      // Update "previous values" with current
+      twist_timestamp = message_time;
+      marker_to_cam_prev = marker_to_cam;
+      markerToCameraPrev = markerToCamera;
 
-    if (publish_fiducial_tf)
+      odom.header.frame_id = origin_frame_id;
+      odom.header.stamp = message_time;
+      odom.child_frame_id = marker_frame_id; // !!!
+      odom.pose.pose.position.x  = odomTransform.translation.x;
+      odom.pose.pose.position.y  = odomTransform.translation.y;
+      odom.pose.pose.position.z  = odomTransform.translation.z;
+      odom.pose.pose.orientation = odomTransform.rotation;
+      odom.twist.twist.linear.x  = robotDisplacementTransform.translation.x / twist_dt_sec;
+      odom.twist.twist.linear.y  = robotDisplacementTransform.translation.y / twist_dt_sec;
+      odom.twist.twist.linear.z  = robotDisplacementTransform.translation.z / twist_dt_sec;
+      odom.twist.twist.angular.x = omega.x;
+      odom.twist.twist.angular.y = omega.y;
+      odom.twist.twist.angular.z = omega.z;
+      odom_pub.publish(odom);
+    }
+
+    if (publish_odom_tf)
     {
       geometry_msgs::TransformStamped transformStamped;
       transformStamped.header.stamp = message_time;
-      transformStamped.header.frame_id = camera_frame_id;
-      transformStamped.child_frame_id = marker_frame_id;
-      transformStamped.transform.translation.x = roboTransform.translation.x;
-      transformStamped.transform.translation.y = roboTransform.translation.y;
-      transformStamped.transform.translation.z = roboTransform.translation.z;
-      transformStamped.transform.rotation = roboTransform.rotation;
-      tf_broadcaster->sendTransform(transformStamped);
-      transformStamped.child_frame_id = origin_frame_id;
-      transformStamped.transform.translation.x = roboTransformOrigo.translation.x;
-      transformStamped.transform.translation.y = roboTransformOrigo.translation.y;
-      transformStamped.transform.translation.z = roboTransformOrigo.translation.z;
-      transformStamped.transform.rotation = roboTransformOrigo.rotation;
+      transformStamped.header.frame_id = origin_frame_id;
+      transformStamped.child_frame_id = footprint_frame_id; // = marker_frame_id if no urdf avail.
+      transformStamped.transform.translation.x = odomTransform.translation.x;
+      transformStamped.transform.translation.y = odomTransform.translation.y;
+      transformStamped.transform.translation.z = odomTransform.translation.z;
+      transformStamped.transform.rotation = odomTransform.rotation;
       tf_broadcaster->sendTransform(transformStamped);
     }
+    else if (publish_fiducial_tf)
+    {
+      geometry_msgs::TransformStamped transformStamped;
+
+      transformStamped.header.stamp = message_time;
+
+      transformStamped.header.frame_id = camera_frame_id;
+      transformStamped.child_frame_id = marker_frame_id;
+      transformStamped.transform.translation.x = markerToCamera.translation.x;
+      transformStamped.transform.translation.y = markerToCamera.translation.y;
+      transformStamped.transform.translation.z = markerToCamera.translation.z;
+      transformStamped.transform.rotation = markerToCamera.rotation;
+      tf_broadcaster->sendTransform(transformStamped);
+
+      transformStamped.child_frame_id = origin_frame_id;
+      transformStamped.transform.translation.x = transformOrigin.translation.x;
+      transformStamped.transform.translation.y = transformOrigin.translation.y;
+      transformStamped.transform.translation.z = transformOrigin.translation.z;
+      transformStamped.transform.rotation = transformOrigin.rotation;
+      tf_broadcaster->sendTransform(transformStamped);
+    }
+
   }
 }
 
@@ -186,14 +210,21 @@ void fiducialTransFormArrayUpdate(const fiducial_msgs::FiducialTransformArray &f
     // If ground marker is present, update its transform
     bool update_required = false;
     if (origin_idx >= 0 && originNeedsUpdate) {
-      roboTransformOrigo = ftf_array.transforms[origin_idx].transform;
+      transformOrigin = ftf_array.transforms[origin_idx].transform;
       originNeedsUpdate = false;
       ROS_INFO_STREAM("Origin set using ground marker (ID = " << origin_id << ")");
+      trackingEnabled = true;
     }
     // If robot marker is present, update its transform and publish updated messages
     if (robot_idx >= 0) {
+      if (originNeedsUpdate) {
+        originNeedsUpdate = false;
+        transformOrigin = markerToCamera;
+        ROS_INFO_STREAM("Origin set using robot marker (ID = " << robot_id << ")");
+        trackingEnabled = true;
+      }
+      markerToCamera = ftf_array.transforms[robot_idx].transform;
       update_required = true;
-      roboTransform = ftf_array.transforms[robot_idx].transform;
     }
     if (update_required) update_messages(ftf_array.header.stamp);
   }
@@ -220,32 +251,49 @@ int main(int argc, char ** argv)
     nhLocal.param("footprint_frame", footprint_frame_id, std::string("base_footprint"));
     nhLocal.param("publish_markers", publish_visualisation_markers, false);
     nhLocal.param("publish_fiducial_tf", publish_fiducial_tf, true);
-    nhLocal.param("marker_to_footprint_from_urdf", wait_for_urdf, false);
+    nhLocal.param("publish_odom_tf", publish_odom_tf, false);
+    nhLocal.param("publish_odometry", publish_odometry, true);
+    nhLocal.param("marker_to_footprint_from_urdf", marker_to_footprint_from_urdf, false);
 
     ros::Subscriber ftf_sub    = n.subscribe("/fiducial_transforms", 100, fiducialTransFormArrayUpdate);
     ros::ServiceServer service = n.advertiseService("/fiducial_tracker/reset_origin", resetRobotOrigin);
 
-    if (publish_visualisation_markers) marker_pub = n.advertise<visualization_msgs::Marker>("/fiducial_markers", 10);
     odom_pub = n.advertise<nav_msgs::Odometry>("/fiducial_tracker/odom", 50);
 
-    if (wait_for_urdf)
-    {
-      while (n.ok())
-      {
+    if (publish_visualisation_markers) {
+      marker_pub = n.advertise<visualization_msgs::Marker>("/fiducial_markers", 10);
+    }
+
+    if (marker_to_footprint_from_urdf) {
+      uint16_t tf_give_up_counter = 0;
+      while (n.ok()) {
          geometry_msgs::TransformStamped transformStamped;
-         try
-         {
+         try {
            transformStamped = tf_buffer.lookupTransform(marker_frame_id, footprint_frame_id, ros::Time(0));
            markerToFootprint = transformStamped.transform;
-           ROS_INFO_STREAM("Got " << marker_frame_id << " to " << footprint_frame_id << " from URDF.");
+           ROS_INFO_STREAM("Got " << marker_frame_id << " to " << footprint_frame_id
+            << " [X: "  << markerToFootprint.translation.x
+            << "] [Y: " << markerToFootprint.translation.y
+            << "] [Z: " << markerToFootprint.translation.z << "]"
+          );
            break;
          }
          catch (tf2::TransformException &ex) {
+           tf_give_up_counter++;
+           if (tf_give_up_counter > 10) {
+             ROS_ERROR_STREAM("Waiting for " << marker_frame_id << " to " << footprint_frame_id << " has timed out.");
+             footprint_frame_id = marker_frame_id;
+             marker_to_footprint_from_urdf = false;
+             break;
+           }
            ROS_WARN("%s",ex.what());
            ros::Duration(1.0).sleep();
            continue;
          }
       }
+    }
+    else {
+      footprint_frame_id = marker_frame_id;
     }
 
     twist_timestamp = ros::Time::now();
